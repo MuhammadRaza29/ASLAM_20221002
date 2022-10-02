f9e73df485fbd3e50f7e015be6c8e4ba736e3649
MuhammadRazaAslam <raza@sendoso.com>
2022-10-02 22:55:45 +0500

    Add categories table

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(email: 'john@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+
+
+['Exercise', 'Education', 'Recipe'].each do |category_name|
+  Category.find_or_create_by(name: category_name)
+end
